e4f0cb852bacc00dc6ed97d4f353de07c41aa21d
Thaís Couto <tvc@cin.ufpe.br>
2023-04-03 20:32:16 -0300

    adding shoot method

--- player.py
+++ player.py
@@ -1,18 +1,20 @@
+import string
+
 class Player():
     def __init__(self, board: Board, ships: list(Ship)):
         self.board = board
         self.ships = []
 
-    def shoot(pos: tuple(int, int)):
-        pass
+    def shoot(letter: string, number: int):
+        return self.board.shoot(string.ascii_uppercase.index(letter)+1, number)
 
-    def hasShip(self, pos: tuple(int, int)):
+    def hasShip(self, pos: int[2]):
         for ship in self.ships:
             if pos == self.__shipsPositions(ship):
                 return True
         return False
 
-    def __shipsPositions(self, ship: Ship) -> list(tuple(int, int)):
+    def __shipsPositions(self, ship: Ship) -> list(int[2]):
         if ship.orientation == 0:  # Horizontal
             return [(ship.position[0], ship.position[1] + i) for i in range(ship.size)]
         elif ship.orientation == 1:  # Vertical
