84eb577ba74f40e439bcd8e2d6ff3e893fe918fe
jailsonsdsj <jailson.junior36@hotmail.com>
2023-04-03 20:27:34 -0300

    Start displayBoard method

--- board.py
+++ board.py
@@ -2,10 +2,14 @@
 class Board:
     def __init__(self, size):
         self.size = size
-        self.letters = [chr(i) for i in range(65, 65 + size)]
-        print(self.letters)
+        self.boardA = [chr(i) for i in range(65, 65 + size)]
+        self.boardB = [chr(i) for i in range(65, 65 + size)]
         # self.board = [[0 for i in range(size * 2)] for j in range(size)]
 
+    def displayBoard(self):
+        print(self.boardA, " | ", self.boardB)
+        
 
 if __name__ == "__main__":
     board = Board(10)
+    board.displayBoard()
