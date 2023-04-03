4b5effbf9fcbf1e8bcc48e3680bc10fac61a5eca
jailsonsdsj <jailson.junior36@hotmail.com>
2023-04-03 20:21:03 -0300

    create top board

--- board.py
+++ board.py
@@ -1,2 +1,11 @@
+
 class Board:
-    pass
+    def __init__(self, size):
+        self.size = size
+        self.letters = [chr(i) for i in range(65, 65 + size)]
+        print(self.letters)
+        # self.board = [[0 for i in range(size * 2)] for j in range(size)]
+
+
+if __name__ == "__main__":
+    board = Board(10)
