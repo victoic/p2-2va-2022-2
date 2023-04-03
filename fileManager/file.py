ba6393382bec59882c7a07570f65b18e42d2f596
isabela menezes <iclm@cin.ufpe.br>
2023-04-03 19:47:04 -0300

    adicionados métodos de operate para classe File

--- fileManager/file.py
+++ fileManager/file.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+import os
 
 class File(ABC):
     def __init__(self, path, filename):
         self.path = path
-        self,filename = filename
+        self.filename = filename
     
     @abstractmethod
     def operate(self):
@@ -14,11 +15,20 @@ class FileReader(File):
         super().__init__(path, filename)
     
     def operate(self):
-        pass
+        try:
+            with open(os.path.join(self.path, self.filename), 'r') as f:
+                return f.read()
+        except:
+            print('file doesnt exist')
 
 class FileWriter(File):
     def __init__(self, path, filename):
         super().__init__(path, filename)
     
     def operate(self):
-        pass
+        try:
+            with open(os.path.join(self.path, self.filename), 'w') as f:
+                f.write()
+                return True
+        except:
+            return False
