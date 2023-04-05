e942c6610a9cb340fa1591f4bd586f9569b60eb9
Gabriela Freitas <gchf@cin.ufpe.br>
2023-04-05 20:19:51 -0300

    added getters and setters to all variables

--- cell.py
+++ cell.py
@@ -8,8 +8,20 @@ class Cell():
         if self.hidden == True:
             return '?'
         if self.shot == True:
-            return 'X'         
+            return 'X'
         return self.symbol
     
     def setSymbol(self, symbol):
         self.symbol = symbol
+
+    def getHidden(self):
+        return self.hidden
+
+    def setHidden(self, hidden):
+        self.hidden = hidden
+
+    def getShot(self):
+        return self.shot
+
+    def setShot(self, shot):
+        self.shot = shot
