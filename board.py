447d1463790c0b8c25e7807c1bbd6adcc07f6558
jailsonsdsj <jailson.junior36@hotmail.com>
2023-04-05 20:03:12 -0300

    create Board class and finish displayBoard method

--- board.py
+++ board.py
@@ -1,15 +1,29 @@
-
 class Board:
-    def __init__(self, size):
-        self.size = size
-        self.boardA = [chr(i) for i in range(65, 65 + size)]
-        self.boardB = [chr(i) for i in range(65, 65 + size)]
-        # self.board = [[0 for i in range(size * 2)] for j in range(size)]
+    # Singleton
+    _instance = None
 
+    def __new__(cls, size):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.size = size
+            cls._instance.board = [[Cell() for i in range(2 * size)] for j in range(size)]
+            return cls._instance
+        else:
+            return Exception("Board already exists")
+
+    
     def displayBoard(self):
-        print(self.boardA, " | ", self.boardB)
-        
-
-if __name__ == "__main__":
-    board = Board(10)
-    board.displayBoard()
+        letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        print('  ', end='')
+        for i in range(self.size): print(' ' + letters[i], end='')
+        print('   ', end='')
+        for i in range(self.size): print(' ' + letters[i], end='')
+        print()
+        for i in range(self.size):
+            print(str(i + 1).rjust(2) + ' ', end='')
+            for j in range(2 * self.size):
+                if j == self.size: print('   ', end='')
+                cell = self.board[i][j]
+                symbol = cell.getSymbol()
+                print(symbol, end=' ')
+            print()
