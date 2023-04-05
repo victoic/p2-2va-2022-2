01667c64343adb9489b032720840bb18f7e8ff31
jgfn1 <jgfn1@cin.ufpe.br>
2023-04-05 19:25:10 -0300

    fixing constructor setting

--- ship.py
+++ ship.py
@@ -4,4 +4,4 @@ class Ship:
         self.position=position
         self.size = size      
         self.orientation = orientation   # 0 para horizontal, 1 para vertical
-        self.hp = hp[size]
+        self.hp = hp
