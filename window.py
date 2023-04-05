fd982840daac065da9f18c0d2161bf3dcc7a8ead
Lucas Leandro da Silva <101291481+LucasLeoSil@users.noreply.github.com>
2023-04-05 20:22:38 -0300

    Create window.py

---
+++ window.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Window(ABC):
+    def __init__(self, file_reader, file_writer):
+        self.history = ''
+        self.file_reader = file_reader
+        self.file_writer = file_writer
+        
+    def checkAction(self):
+        file_content = self.file_reader.operate()
+
+        if file_content != self.history:
+            self.history = file_content
+            return True
+        
+        return False
+        
