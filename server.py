307d3226032ee64514d5683c0737676bbf661c9e
Pedro Souza <32937084+pjcsouza@users.noreply.github.com>
2023-04-05 20:36:55 -0300

    Update server.py

--- server.py
+++ server.py
@@ -20,14 +20,41 @@ class Server(Window):
         self.__p1_writer.operate("", "w")
         self.__p2_writer.operate("", "w")
 
-    def checkCheating(self):
-        pass
-    
     def checkAction(self):
-        pass
+        if self.currentTurn == 1:
+            file_content = self.file_reader_p1.operate()
+        else:
+            file_content = self.file_reader_p2.operate()
+        if file_content != self.history[self.currentTurn]:
+            self.history = file_content
+            return True
+        return False 
 
-    def writeAction(self):
-        pass
+    def writeAction(self, action):
+        self.server_writer.operate(action)
+
+    def _getLastLineTokens(self):
+        lines = self.history.splitlines()
+        last_line = lines[-1]
+
+        return last_line
 
     def gameLoop(self):
-        pass
+        while True:
+            if self.checkAction():
+                last_line = self._getLastLineTokens()
+                
+                self.writeAction(last_line)
+                if self.currentTurn == 1:
+                    self.currentTurn = 2
+                else:
+                    self.currentTurn = 1
+    def checkCheating(self):
+        actions_p1 = self.file_reader_p1.operate().splitLines()
+        actions_p2 = self.file_reader_p2.operate().splitlines()
+        history_p1 = self.history[1].splitlines()
+        history_p2 = self.history[2].splitlines()
+
+        cheat_p1 = all([action_history in actions_p1 for action_history in history_p1])
+        cheat_p2 = all([action_history in actions_p2 for action_history in history_p2])
+        return cheat_p1 and cheat_p2
